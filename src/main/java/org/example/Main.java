439f305cf84dc5e465a879e5935e22d8c9f2ed0f
GulkhayoDeveloper <138315833+GulkhayoDeveloper@users.noreply.github.com>
2023-09-30 22:24:23 +0500

    Change (#4)
    Co-authored-by: ASUS <ilovemymomx@gmail.com>
    Co-authored-by: RED <97430176+7Davronbek@users.noreply.github.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("lorem");
         System.out.println("Hello world");
+        System.out.println("jsdcnjsdcjsdd");
         System.out.println("teacher");
         System.out.println();
     }
