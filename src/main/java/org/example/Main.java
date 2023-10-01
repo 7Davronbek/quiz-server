0ff92743d98be00d8eb85bd48860d3edd6de4e4d
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-10-01 20:08:57 +0300

    user created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,5 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Connection connection = Database.getConnection();
     }
 }
