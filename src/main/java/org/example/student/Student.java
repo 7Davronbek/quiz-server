12defb39de6386ce8c1b2df6be4f9fe58053f4f5
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-10-01 09:33:31 +0300

    database created

--- src/main/java/org/example/student/Student.java
+++ src/main/java/org/example/student/Student.java
@@ -1,7 +1,13 @@
 package org.example.student;
 
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private UUID id;
     private String name;
