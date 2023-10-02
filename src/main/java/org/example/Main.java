5646c345e598e79c2a7b50e275e7560201bc3807
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-10-02 19:21:17 +0300

    option and test created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import lombok.SneakyThrows;
 import org.example.ui.BaseUI;
 
 import java.util.Scanner;
@@ -8,6 +9,7 @@ public class Main {
     public static Scanner scannerInt = new Scanner(System.in);
     public static Scanner scannerStr = new Scanner(System.in);
 
+    @SneakyThrows
     public static void main(String[] args) {
         new BaseUI().quizStart();
     }
---
+++ src/main/java/org/example/test/TestCategory.java
@@ -0,0 +1,5 @@
+package org.example.test;
+
+public enum TestCategory {
+    MATH, BIOLOGY, HISTORY
+}
